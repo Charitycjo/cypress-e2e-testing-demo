8ff4b69d246e2428238e1cff71eaf322d2fc1c1d
Charitycjo <166632108+Charitycjo@users.noreply.github.com>
2026-03-03 21:02:05 -0600

    Update Saucedemoworkflow.cy.js

--- cypress/e2e/Saucedemoworkflow.cy.js
+++ cypress/e2e/Saucedemoworkflow.cy.js
@@ -4,14 +4,18 @@ import CartPage from '../support/pages/CartPage';
 import CheckoutPage from '../support/pages/CheckoutPage';
 
 describe('SauceDemo End-to-End Checkout Flow with POM + Custom Commands', () => {
-  beforeEach(function () {
-  cy.fixture('user').then((data) => {
-    this.user = data.standardUser;
-    cy.login(this.user.username, this.user.password);
-  });
-});
 
-  it('Completes checkout for one item', () => {
+  beforeEach(function () {
+    cy.fixture('user').then((data) => {
+      this.user = data.standardUser;
+      this.checkoutInfo = data.checkoutInfo;
+
+      cy.login(this.user.username, this.user.password);
+    });
+  });
+
+  it('Completes checkout for one item', function () {
+
     ProductsPage.addBackpackToCart();
     ProductsPage.assertCartBadgeCount(1);
     ProductsPage.openCart();
@@ -21,17 +25,17 @@ describe('SauceDemo End-to-End Checkout Flow with POM + Custom Commands', () => 
     CartPage.clickCheckout();
 
     CheckoutPage.assertOnCheckoutStepOne();
-    cy.fixture('user').then((data) => {
-  const info = data.checkoutInfo;
-  CheckoutPage.fillCustomerInfo(
-    info.firstName,
-    info.lastName,
-    info.postalCode
-  );
-});
+
+    CheckoutPage.fillCustomerInfo(
+      this.checkoutInfo.firstName,
+      this.checkoutInfo.lastName,
+      this.checkoutInfo.postalCode
+    );
+
     CheckoutPage.clickContinue();
     CheckoutPage.clickFinish();
 
     CheckoutPage.assertOrderComplete();
   });
+
 });
